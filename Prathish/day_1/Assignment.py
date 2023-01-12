42c5c10412b1346adbe1e646a5256dee515c009c
Prat <prathishrsamy@gmail.com>
2023-01-12 16:41:35 +0000

    day_1 submission

---
+++ Prathish/day_1/Assignment.py
@@ -0,0 +1,5 @@
+#1. Create a greeting for your program.
+print("Welcme to the band name generator")
+city=input("City you grew up?\n")
+pet=input("What is your pet Name?\n")
+print(f"The Band name: {city} {pet}")
